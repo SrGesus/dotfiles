5bc62b34dcdfca4d30284f8c7b7a867f3d38eb8a
SrGesus <108523575+SrGesus@users.noreply.github.com>
2025-07-30 22:48:04 +0100

    unstable -> 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS Config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi/main";
     home = {
       inputs.nixpkgs.follows = "nixpkgs";
