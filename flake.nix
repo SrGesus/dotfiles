d59fbf7fc24bdb631b8f471684cb7f1c465db924
SrGesus <108523575+SrGesus@users.noreply.github.com>
2025-07-30 22:45:10 +0100

    Graz first attempt

--- flake.nix
+++ flake.nix
@@ -3,13 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager/release-25.05";
+    };
   };
   
   outputs = { self, nixpkgs, ... }: {
     nixosConfigurations = {
       graz = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
-        modules = [ ./hosts/graz/configuration.nix ];
+        modules = [ ./hosts/graz/default.nix ];
       };
     };
   };
---
+++ hosts/graz/default.nix
@@ -0,0 +1,70 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    ./hardware.nix
+  ];
+
+  networking.hostName = "graz";
+  networking.networkmanager.enable = true;
+  networking.firewall.enable = false;
+  services.openssh.enable = true;
+
+  # Timezone
+  time.timeZone = "Europe/Lisbon";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    # font = "Lat2-Terminus16";
+
+    # Pt Keyboard
+    keyMap = lib.mkForce "pt-latin9";
+    #keyMap = "pt";
+    useXkbConfig = true; # use xkb.options in tty.
+  };
+
+  services.xserver = {
+    enable = true;
+    xkb.layout = "pt";
+    # xkb.options = "eurosign:e,caps:escape";
+  };
+
+  # Enable CUPS to print documents
+  # services.printing.enable = true;
+
+  # Enable sounds.
+  # services.pulseaudio.enable = true;
+  # OR
+  # services.pipewire.enable = true;
+  # services.pipewire.pulse.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    helix
+    eza
+    ripgrep
+    iwd
+    man
+    fastfetch
+  ];
+
+  # user
+  users.users.pi = {
+    initialPassword = "banana";
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+    ];
+  };
+  security.sudo.wheelNeedsPassword = false;
+  nix.settings.trusted-users = [
+    "root"
+    "pi"
+  ];
+
+  system.stateVersion = "25.05";
+}
