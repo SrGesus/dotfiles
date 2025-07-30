5735cd4e22ae363fa3b219228a5b51cacf801a6b
SrGesus <108523575+SrGesus@users.noreply.github.com>
2025-07-30 22:47:25 +0100

    unstable -> 25.05

--- flake.nix
+++ flake.nix
@@ -2,11 +2,21 @@
   description = "My NixOS Config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-25.05";
+    nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi/main";
     home = {
       inputs.nixpkgs.follows = "nixpkgs";
       url = "github:nix-community/home-manager/release-25.05";
     };
+  };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://nixos-raspberrypi.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
+    ];
   };
   
   outputs = { self, nixpkgs, ... }: {
