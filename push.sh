4ff88ba7246bdc72f4666c3b1954cf060b7e50d3
SrGesus <108523575+SrGesus@users.noreply.github.com>
2025-09-07 17:29:08 +0100

    Change zoxide cmd to cd NixOS 2025-09-07T16:29:01+00:00 Build versailles 47

--- push.sh
+++ push.sh
@@ -2,17 +2,17 @@
 
 cd $(dirname "$0") &&
 # Add files to git or else nix will ignore them
-echo "== Adding files to git..." &&
+printf "== Adding files to git...\n" &&
 git add . &&
 
-echo "== Building NixOS..." &&
+printf "== Building NixOS...\n" &&
 sudo nixos-rebuild switch --flake .#$(hostname) &&
 
-echo "== Committing to git..." &&
+printf "== Committing to git...\n" &&
 DEFAULT_COMMIT_MSG="NixOS $(date -u --iso-8601=seconds) Build $(hostname) $(readlink /nix/var/nix/profiles/system | cut -d- -f2)" &&
 read -p "Commit message ($DEFAULT_COMMIT_MSG): " PROMPT_COMMIT_MSG &&
-COMMIT_MSG=$([[ -z "${PROMPT_COMMIT_MSG// /}" ]] && echo $DEFAULT_COMMIT_MSG || echo "$PROMPT_COMMIT_MSG\n$DEFAULT_COMMIT_MSG")
+COMMIT_MSG=$([[ -z "${PROMPT_COMMIT_MSG// /}" ]] && printf $DEFAULT_COMMIT_MSG || printf "$PROMPT_COMMIT_MSG\n$DEFAULT_COMMIT_MSG")
 git commit -m "$COMMIT_MSG" &&
 
-echo "== Pushing to remote..." &&
+printf "== Pushing to remote...\n" &&
 git push
