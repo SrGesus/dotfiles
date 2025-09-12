8d21d0fd131e114d57a55ab7bebfdb2ca51445fc
SrGesus <108523575+SrGesus@users.noreply.github.com>
2025-09-12 20:32:22 +0100

    NixOS

--- hosts/versailles/default.nix
+++ hosts/versailles/default.nix
@@ -120,6 +120,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     unrar
+    kdePackages.kdeconnect-kde
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
