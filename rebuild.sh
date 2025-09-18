6176be5427979014b5495396a3501009149072d2
SrGesus <108523575+SrGesus@users.noreply.github.com>
2025-09-19 00:46:10 +0100

    Add readline to script commit msg read

--- rebuild.sh
+++ rebuild.sh
@@ -10,7 +10,7 @@ sudo nixos-rebuild switch --flake .#$(hostname) &&
 
 printf "== Committing to git...\n" &&
 DEFAULT_COMMIT_MSG="NixOS $(date -u --iso-8601=seconds) Build $(hostname) $(readlink /nix/var/nix/profiles/system | cut -d- -f2)" &&
-read -p "Commit message ($DEFAULT_COMMIT_MSG): " PROMPT_COMMIT_MSG &&
+read -ep "Commit message ($DEFAULT_COMMIT_MSG): " PROMPT_COMMIT_MSG &&
 COMMIT_MSG=$([[ -z "${PROMPT_COMMIT_MSG// /}" ]] && printf "$DEFAULT_COMMIT_MSG" || printf "$PROMPT_COMMIT_MSG\n$DEFAULT_COMMIT_MSG")
 git commit -m "$COMMIT_MSG" &&
 
