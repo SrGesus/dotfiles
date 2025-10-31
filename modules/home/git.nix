4d5a775a49e74f992197294b67c89b347d9cadba
SrGesus <108523575+SrGesus@users.noreply.github.com>
2025-10-31 18:29:39 +0000

    Update and fix deprecated options NixOS 2025-10-31T18:29:30+00:00 Build versailles 87

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,7 +1,9 @@
-{ lib, config, ... }: let
+{ lib, config, ... }:
+let
   cfg = config.modules.git;
   inherit (lib) mkIf mkEnableOption;
-in {
+in
+{
   options.modules.git = {
     enable = mkEnableOption "git";
     # delta = mkEnableOption "git delta";
@@ -9,20 +11,24 @@ in {
 
   config.programs.git = mkIf cfg.enable {
     enable = true;
-    
-    userName = "SrGesus";
-    userEmail = "108523575+SrGesus@users.noreply.github.com";
 
-    delta = {
-      enable = true;
-        options = {
-          features = "decorations";
-          line-numbers = true;
-          # Workaround for https://github.com/dandavison/delta/issues/1663
-          # dark = true;
-        };
+    settings = {
+      user.name = "SrGesus";
+      user.email = "108523575+SrGesus@users.noreply.github.com";
     };
 
     lfs.enable = true;
+  };
+
+  config.programs.delta = mkIf cfg.enable {
+    enable = true;
+    enableGitIntegration = true;
+
+    options = {
+      features = "decorations";
+      line-numbers = true;
+      # Workaround for https://github.com/dandavison/delta/issues/1663
+      # dark = true;
+    };
   };
 }
