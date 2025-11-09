4e6cd81d5713839ae1ac1a1fb727430137eb4d3c
SrGesus <108523575+SrGesus@users.noreply.github.com>
2025-11-09 12:10:32 +0000

    NixOS 2025-11-09T12:10:07+00:00 Build versailles 90

--- modules/home/games.nix
+++ modules/home/games.nix
@@ -1,15 +1,31 @@
-{ config, pkgs, myLib, lib, ... }:
+{
+  config,
+  pkgs,
+  myLib,
+  lib,
+  ...
+}:
 let
   cfg = config.modules.games;
   inherit (myLib) mkIfList;
   inherit (lib) mkEnableOption;
-in {
-  options.modules.games = { minecraft = mkEnableOption "minecraft"; };
+in
+{
+  options.modules.games = {
+    minecraft = mkEnableOption "minecraft";
+  };
 
-  config.home.packages = (mkIfList cfg.minecraft [ pkgs.prismlauncher ]) ++ [ pkgs.wineWow64Packages.full ];
+  config.home.packages = (mkIfList cfg.minecraft [ pkgs.prismlauncher ]) ++ [
+    pkgs.wineWow64Packages.full
+    pkgs.winetricks
+  ];
 
   config.programs.lutris = {
     enable = true;
     winePackages = [ pkgs.wineWow64Packages.full ];
+    extraPackages = with pkgs; [
+      libadwaita
+      gtk4
+    ];
   };
 }
