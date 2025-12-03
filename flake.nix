cde7cdd375f788a6867e9ae4b67f5fbcb8165364
SrGesus <108523575+SrGesus@users.noreply.github.com>
2025-12-03 12:49:44 +0000

    NixOS 2025-12-03T12:49:42+00:00 Build versailles 101

--- flake.nix
+++ flake.nix
@@ -10,6 +10,12 @@
     };
   };
 
+  nixConfig = {
+    experimental-features = [ "nix-command" "flakes" ];
+    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+  };
+
   outputs = inputs@{ self, nixpkgs, ... }:
   let
       inherit (inputs.nixpkgs) lib;
