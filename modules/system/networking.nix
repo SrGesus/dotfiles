9bfe451deb71895a384ecfb8baa0a9b491d4e87b
SrGesus <108523575+SrGesus@users.noreply.github.com>
2025-12-22 16:49:48 +0000

    NixOS 2025-12-22T16:12:06+00:00 Build versailles 103

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -1,5 +1,12 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   services.tailscale.enable = true;
+
+  services.resolved.enable = true;
+  services.resolved.dnssec = "true"; # Or "allow-downgrade"
+  # Optional: Set trusted DNS servers
+  networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
+  networking.networkmanager.dns = lib.mkForce "none";
+
 
   # boot.kernelPatches = [
   #   # Fix the /proc/net/tcp seek issue
